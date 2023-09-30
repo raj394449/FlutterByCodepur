18d6c39715a49e6ac95ae3f73910d00d5d1d1c87
Sanjay <sanjayraj394449@gmail.com>
2023-09-30 14:25:42 +0530

    NoChanges

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
